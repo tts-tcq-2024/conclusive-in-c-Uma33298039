f34683d3b7b177b807cb2895ae2ca6b3e286622f
Uma33298039 <umamageshwari098@gmail.com>
2024-08-23 16:36:37 +0530

    Update and rename sendToController.c to email_alert.cpp

--- sendToController.c
+++
@@ -1,7 +0,0 @@
-#include "sendToController.h"
-#include <stdio.h>
-
-void sendToController(BreachType breachType) {
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-}
