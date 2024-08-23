67e828895ac78305b65bc19a0f83ee143009b49e
Uma33298039 <umamageshwari098@gmail.com>
2024-08-23 16:31:00 +0530

    Update and rename ClassifyTemperatureBreach.cpp to alert_checker.cpp

--- ClassifyTemperatureBreach.cpp
+++
@@ -1,7 +0,0 @@
-#include "classifyTemperatureBreach.h"
-
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
-  int lowerLimit, upperLimit;
-  getCoolingLimits(coolingType, &lowerLimit, &upperLimit);
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
-}
