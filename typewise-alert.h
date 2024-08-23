9a427e154b9378e6c576268447da98c050b1fb4c
Uma33298039 <umamageshwari098@gmail.com>
2024-08-23 14:08:53 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -12,9 +12,6 @@ typedef enum {
   TOO_HIGH
 } BreachType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
-
 typedef enum {
   TO_CONTROLLER,
   TO_EMAIL
@@ -24,9 +21,3 @@ typedef struct {
   CoolingType coolingType;
   char brand[48];
 } BatteryCharacter;
-
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
-
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
