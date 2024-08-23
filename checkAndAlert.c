955964d2d7b880906a1e8b3aeaf0b23d4773dcc2
Uma33298039 <umamageshwari098@gmail.com>
2024-08-23 16:28:35 +0530

    Update and rename checkAndAlert.c to checkAndAlert.cpp

--- checkAndAlert.c
+++
@@ -1,6 +0,0 @@
-#include "checkAndAlert.h"
-
-void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-  BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
-  alertBasedOnTarget(alertTarget, breachType);
-}
