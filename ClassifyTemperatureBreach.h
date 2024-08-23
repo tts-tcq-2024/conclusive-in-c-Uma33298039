da5d7bd8ca729475de8af465d80e9c6a71498556
Uma33298039 <umamageshwari098@gmail.com>
2024-08-23 16:33:54 +0530

    Update and rename ClassifyTemperatureBreach.h to battery_character.h

--- ClassifyTemperatureBreach.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "typewise-alert.h"
-#include "getCoolingLimits.h"
-#include "inferBreach.h"
-
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
