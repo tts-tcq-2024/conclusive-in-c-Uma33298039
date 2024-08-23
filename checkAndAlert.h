91b70e012c05a9e621bd32b9b8569a331cdbd2a5
Uma33298039 <umamageshwari098@gmail.com>
2024-08-23 16:29:18 +0530

    Update and rename checkAndAlert.h to alert_target.h

--- checkAndAlert.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "typewise-alert.h"
-#include "classifyTemperatureBreach.h"
-#include "alertBasedOnTarget.h"
-
-void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
