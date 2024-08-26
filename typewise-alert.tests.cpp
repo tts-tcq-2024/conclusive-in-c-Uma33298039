fbbcd00188c91fb9a3ce5d74acfc233a3033c873
Uma33298039 <umamageshwari098@gmail.com>
2024-08-26 09:43:07 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,55 +1,114 @@
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+#include "AlertInEmail.h"
+#include "TemperatureAlert_checker.h"
 #include "breach_detector.h"
-#include "alert_checker.h"
-#include "message_sender_to_controller.h"
-#include "message_sender_to_email.h"
+#include "alert_to_controller.h"
+#include "cooling_config.h"
+#include <string>
+#include <sstream>
 
-TEST(TypeWiseAlertTestSuite, InfersBreachAccordingToLimits) {
-    EXPECT_EQ(infer_breach(12, 20, 30), TOO_LOW);
+// Helper class to capture printf output
+class PrintfCapture {
+public:
+    PrintfCapture() {
+        old_stdout = std::cout.rdbuf(buffer.rdbuf());
+    }
+
+    ~PrintfCapture() {
+        std::cout.rdbuf(old_stdout);
+    }
+
+    std::string str() const {
+        return buffer.str();
+    }
+
+private:
+    std::ostringstream buffer;
+    std::streambuf* old_stdout;
+};
+
+// Test for send_to_email
+TEST(AlertInEmail, TestSendToEmailLowBreach) {
+    PrintfCapture capture;
+    send_to_email(LOW_BREACH);
+    std::string expected = "To: a.b@c.com\nHi, the temperature is too low\n";
+    EXPECT_EQ(capture.str(), expected);
+}
+
+TEST(AlertInEmail, TestSendToEmailHighBreach) {
+    PrintfCapture capture;
+    send_to_email(HIGH_BREACH);
+    std::string expected = "To: a.b@c.com\nHi, the temperature is too high\n";
+    EXPECT_EQ(capture.str(), expected);
+}
+
+TEST(AlertInEmail, TestSendToEmailNormal) {
+    PrintfCapture capture;
+    send_to_email(NORMAL);
+    std::string expected = "To: a.b@c.com\n";
+    EXPECT_EQ(capture.str(), expected);
+}
+
+// Test for send_to_controller
+TEST(AlertToController, TestSendToController) {
+    PrintfCapture capture;
+    send_to_controller(TOO_LOW);
+    std::string expected = "feed : 1\n"; // Assuming breach_type TOO_LOW == 1
+    EXPECT_EQ(capture.str(), expected);
+}
+
+// Test for breach_detector
+TEST(BreachDetector, TestInferBreachTooLow) {
+    EXPECT_EQ(infer_breach(10, 20, 30), TOO_LOW);
+}
+
+TEST(BreachDetector, TestInferBreachTooHigh) {
+    EXPECT_EQ(infer_breach(40, 20, 30), TOO_HIGH);
+}
+
+TEST(BreachDetector, TestInferBreachNormal) {
     EXPECT_EQ(infer_breach(25, 20, 30), NORMAL);
-    EXPECT_EQ(infer_breach(34, 20, 30), TOO_HIGH);
 }
 
-TEST(TypeWiseAlertTestSuite, ClassifiesTemperatureBreachForCoolingTypes) {
-    EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, 36), TOO_HIGH);
-    EXPECT_EQ(classify_temperature_breach(HI_ACTIVE_COOLING, 46), TOO_HIGH);
-    EXPECT_EQ(classify_temperature_breach(MED_ACTIVE_COOLING, 41), TOO_HIGH);
-    EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, 20), NORMAL);
+TEST(BreachDetector, TestClassifyTemperatureBreach) {
+    EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, 50), TOO_HIGH);
+    EXPECT_EQ(classify_temperature_breach(HI_ACTIVE_COOLING, -5), TOO_LOW);
+    EXPECT_EQ(classify_temperature_breach(MED_ACTIVE_COOLING, 20), NORMAL);
 }
 
-TEST(TypeWiseAlertTestSuite, SendsAlertToController) {
-    testing::internal::CaptureStdout();
-    send_to_controller(TOO_HIGH);
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "feed : 2\n");
+// Test for check_and_alert
+TEST(TemperatureAlertChecker, TestCheckAndAlertToController) {
+    PrintfCapture capture;
+    battery_character battery = {PASSIVE_COOLING, "Battery 1"};
+    check_and_alert(TO_CONTROLLER, battery, 50);
+    std::string expected = "feed : 2\n"; // Assuming breach_type TOO_HIGH == 2
+    EXPECT_EQ(capture.str(), expected);
 }
 
-TEST(TypeWiseAlertTestSuite, SendsAlertToEmail) {
-    testing::internal::CaptureStdout();
-    send_to_email(TOO_LOW);
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "To: a.b@c.com\nHi, the temperature is too low\n");
-
-    testing::internal::CaptureStdout();
-    send_to_email(TOO_HIGH);
-    output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "To: a.b@c.com\nHi, the temperature is too high\n");
+TEST(TemperatureAlertChecker, TestCheckAndAlertToEmail) {
+    PrintfCapture capture;
+    battery_character battery = {HI_ACTIVE_COOLING, "Battery 2"};
+    check_and_alert(TO_EMAIL, battery, -5);
+    std::string expected = "To: a.b@c.com\nHi, the temperature is too low\n";
+    EXPECT_EQ(capture.str(), expected);
 }
 
-TEST(TypeWiseAlertTestSuite, CheckAndAlertToController) {
-    BatteryCharacter batteryChar = {PASSIVE_COOLING, "BrandX"};
+// Test for cooling_config
+TEST(CoolingConfig, TestGetCoolingConfig) {
+    cooling_config config = get_cooling_config(PASSIVE_COOLING);
+    EXPECT_EQ(config.lower_limit, PASSIVE_COOLING_LOWER_LIMIT);
+    EXPECT_EQ(config.upper_limit, PASSIVE_COOLING_UPPER_LIMIT);
     
-    testing::internal::CaptureStdout();
-    check_and_alert(TO_CONTROLLER, batteryChar, 36);
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "feed : 2\n");
+    config = get_cooling_config(HI_ACTIVE_COOLING);
+    EXPECT_EQ(config.lower_limit, HI_ACTIVE_COOLING_LOWER_LIMIT);
+    EXPECT_EQ(config.upper_limit, HI_ACTIVE_COOLING_UPPER_LIMIT);
+    
+    config = get_cooling_config(MED_ACTIVE_COOLING);
+    EXPECT_EQ(config.lower_limit, MED_ACTIVE_COOLING_LOWER_LIMIT);
+    EXPECT_EQ(config.upper_limit, MED_ACTIVE_COOLING_UPPER_LIMIT);
 }
 
-
-TEST(TypeWiseAlertTestSuite, CheckAndAlertToEmail) {
-    BatteryCharacter batteryChar = {HI_ACTIVE_COOLING, "BrandY"};    
-    testing::internal::CaptureStdout();
-    check_and_alert(TO_EMAIL, batteryChar, 46);
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "To: a.b@c.com\nHi, the temperature is too high\n");
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
