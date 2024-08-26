6af3a323a4ef6fb9b04eeabd8ac2ee60d5aad693
Uma33298039 <umamageshwari098@gmail.com>
2024-08-26 09:31:09 +0530

    Update AlertInEmail.h

--- AlertInEmail.h
+++ AlertInEmail.h
@@ -1,5 +1,12 @@
-#pragma once
+#ifndef ALERT_IN_EMAIL_H
+#define ALERT_IN_EMAIL_H
 
-#include "breach_type.h"
+enum breach_type {
+    NORMAL,
+    LOW_BREACH,
+    HIGH_BREACH
+};
 
 void send_to_email(breach_type breach);
+
+#endif // ALERT_IN_EMAIL_H
